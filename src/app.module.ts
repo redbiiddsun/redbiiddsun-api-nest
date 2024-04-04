bee905357d261ea7f24aabc357f6012992c5baf7
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-04-05 00:35:44 +0700

    feat: PrismaModule to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,32 @@ import { AppService } from './app.service';
 
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
   imports: [
+    // ConfigModule.forRoot() is a global module that loads environment variables from a .env file
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
+
+    // PrismaModule.forRoot() is a global module that connects to the Prisma client
+    PrismaModule.forRoot({
+      isGlobal: true,
+      prismaServiceOptions: {
+        explicitConnect: true,
+        prismaOptions: {
+          log: [
+            {
+              emit: 'event',
+              level: 'query',
+            },
+          ],
+        },
+      },
+    }),
+
     UserModule,
   ],
   controllers: [AppController],
