5c934ee59d7817966478f84f6bcfb7f6ffc0e0f4
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-07-22 00:44:24 +0700

    chore: remove unused configuration

--- src/config/configuration.ts
+++
@@ -1,5 +0,0 @@
-export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
-  salt_rounds: parseInt(process.env.SALT_ROUNDS) || 10,
-  jwt_secret: process.env.JWT_SECRET || 'secret',
-});
