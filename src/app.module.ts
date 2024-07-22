a9449742049cefdb2a17d61b0cd368d0ab804a99
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-07-22 22:19:53 +0700

    feat: add TypeOrmModule in import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,28 @@
 import { Module } from '@nestjs/common';
+import 'dotenv/config';
 
-import configuration from './config/configuration';
 import { ConfigModule } from '@nestjs/config';
 import { ThaiIdModule } from './thai-id/thai-id.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { mysqlConfiguration } from 'typeorm-config';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
-    // ConfigModule.forRoot() is a global module that loads environment variables from a configuration file
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [configuration],
     }),
+
+    TypeOrmModule.forRootAsync({
+      useFactory() {
+        return { ...mysqlConfiguration, autoLoadEntities: true };
+      },
+      dataSourceFactory(mysqlConfiguration) {
+        return Promise.resolve(new DataSource(mysqlConfiguration));
+      },
+    }),
+
     ThaiIdModule,
   ],
 })
